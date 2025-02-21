3e5b02efdd72d080306ffeaade87964625350f25
pprunty <pprunty@tcd.ie>
2025-02-21 15:13:41 +0000

    test

--- src/app/(main)/user-button.tsx
+++ src/app/(main)/user-button.tsx
@@ -2,9 +2,6 @@ import Image from 'next/image';
 import { auth } from '../../auth';
 import { SignIn, SignOut } from './auth-components';
 
-import InitialsAvatar from 'react-initials-avatar';
-import 'react-initials-avatar/lib/ReactInitialsAvatar.css';
-
 export default async function UserButton() {
   const session = await auth();
 
@@ -15,19 +12,13 @@ export default async function UserButton() {
       <span className="hidden text-sm sm:inline-flex">
         {session.user.email}
       </span>
-      {session.user.image ? (
-        <Image
-          src={session.user.image}
-          alt={session.user.name ?? 'User'}
-          width={40}
-          height={40}
-          className="rounded-full"
-        />
-      ) : (
-        <InitialsAvatar
-          name={session.user.name || session.user.email || 'User'}
-        />
-      )}
+      <Image
+        src={session.user.image ?? '/default-avatar.png'}
+        alt={session.user.name ?? 'User'}
+        width={40}
+        height={40}
+        className="rounded-full"
+      />
       <SignOut className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors" />
     </div>
   );

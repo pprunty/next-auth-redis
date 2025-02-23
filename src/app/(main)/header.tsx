6b0f6253a4b2c66bd3d378d2bb63870d5f28a02f
pprunty <pprunty@tcd.ie>
2025-02-23 23:48:47 +0000

    test

--- src/app/(main)/header.tsx
+++ src/app/(main)/header.tsx
@@ -1,13 +1,16 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import UserButton from './user-button';
 import ThemeSwitcher from '@/app/components/theme-toggle';
 
 export default function Header() {
   return (
     <header className="flex items-center justify-between p-2">
-      <h1 className="text-lg font-mono">
-        <Link href="/">next-auth-redis</Link>
-      </h1>
+      <div className="flex items-center gap-2">
+        <Link href="/">
+          <Image src="/icon.webp" alt="Logo" width={35} height={35} />
+        </Link>
+      </div>
       <div className="flex items-center gap-2">
         <ThemeSwitcher />
         <UserButton />

032a53502e6f824687488d2d768b18436b873e1e
pprunty <pprunty@tcd.ie>
2025-02-23 17:20:41 +0000

    test

--- src/lib/next-auth.ts
+++ src/lib/next-auth.ts
@@ -1,6 +1,6 @@
 // lib/nextAuth.ts
 export async function fetchNextAuthData() {
-  const res = await fetch('https://next-auth-redis.vercel.app/');
+  const res = await fetch('https://vercel-fastapi-backend.vercel.app/');
   if (!res.ok) {
     throw new Error('Error fetching data from next-auth-redis');
   }
